e5178a9c1564bfcb8b9b4d294802a46e85d55a6b
Aman7080 <kumar7080aman7080@gmail.com>
2024-10-08 23:22:49 +0530

    hello world api

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new().route("/", get(|| async { "hello world" }));
+    let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
