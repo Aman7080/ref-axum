d7a87a25476a7bae484d5bb7f317872c2490aae3
Aman7080 <kumar7080aman7080@gmail.com>
2024-10-08 23:28:52 +0530

    separate function for handler

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app: Router = Router::new().route("/", get(|| async { "hello world" }));
+    let app: Router = Router::new().route("/", get(hello_world));
     let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+
+async fn hello_world() -> String{
+     "hello world".to_owned()
 }
