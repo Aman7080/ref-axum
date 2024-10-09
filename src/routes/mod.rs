b29a4ce95cc505b95f21e5bbcedd147df8e4b559
Aman7080 <kumar7080aman7080@gmail.com>
2024-10-09 22:26:05 +0530

    path parameter added

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,10 +1,12 @@
 mod hello_world;
 mod get_body_text;
 mod body_json;
+mod path_params;
 use axum::{routing::{get, post}, Router};
 use body_json::{json_body, json_body2};
 use get_body_text::body_text;
 use hello_world::hello_world;
+use path_params::path_param;
 
 pub fn create_routes() -> Router {
     Router::new()
@@ -12,5 +14,6 @@ pub fn create_routes() -> Router {
     .route("/body-text", post(body_text))
     .route("/body-json", post(json_body))
     .route("/body-json2", post(json_body2))
+    .route("/path/:id", get(path_param))
 }
 
