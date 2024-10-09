6c7fc6c3fa899bf6aabfc60a080329fa45a5659a
Aman7080 <kumar7080aman7080@gmail.com>
2024-10-09 23:58:26 +0530

    cors

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -5,15 +5,23 @@ mod path_params;
 mod query_params;
 mod get_headers;
 
-use axum::{routing::{get, post}, Router};
+use axum::{http::Method, routing::{get, post}, Router};
 use body_json::{json_body, json_body2};
 use get_body_text::body_text;
 use get_headers::get_header;
 use hello_world::hello_world;
 use path_params::path_param;
 use query_params::query_params;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_routes() -> Router {
+    
+    let cors = CorsLayer::new()
+    // allow `GET` and `POST` when accessing the resource
+    .allow_methods([Method::GET, Method::POST])
+    // allow requests from any origin
+    .allow_origin(Any);
+
     Router::new()
     .route("/", get(hello_world))
     .route("/body-text", post(body_text))
@@ -22,5 +30,6 @@ pub fn create_routes() -> Router {
     .route("/path/:id", get(path_param))
     .route("/query", get(query_params))
     .route("/headers", get(get_header))
+    .layer(cors)
 }
 
